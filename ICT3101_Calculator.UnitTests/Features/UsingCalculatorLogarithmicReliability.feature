f119fe07b7c76c8c6b655a833458fd629570167f
Greenbonee <nadzim1998@gmail.com>
2023-09-29 03:22:37 +0800

    Log

---
+++ ICT3101_Calculator.UnitTests/Features/UsingCalculatorLogarithmicReliability.feature
@@ -0,0 +1,16 @@
+﻿Feature: UsingCalculatorLogarithmicReliability
+	In order to calculate the Musa Logarithmic model's failures/intensities
+	As a system quality engineer
+	I want to use my calculator to do this
+
+@CurrentLogFailureIntensity
+Scenario: Calculating logarithmic model failure intensity normally
+	Given I have a calculator
+	When I have entered "10", "0.02" and "50" into the calculator and press logarithmic failure intensity
+	Then the log failure intensity result should be "3.68"
+
+@CurrentLogExpectedFailureIntensity
+Scenario: Calculating logarithmic model number of expected failures normally
+	Given I have a calculator
+	When I have entered "10", "0.02" and "10" into the calculator and press logarithmic expected failure
+	Then the log expected failure result should be "54"
---
+++ ICT3101_Calculator.UnitTests/Step_Definitions/UsingCalculatorLogarithmicReliabilitySteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace ICT3101_Calculator.UnitTests.Step_Definitions
+{
+    [Binding]
+    public class UsingCalculatorLogarithmicReliabilitySteps
+    {
+        private decimal _result;
+
+        private readonly CalculatorDetails calculatorDetails;
+
+        public UsingCalculatorLogarithmicReliabilitySteps(CalculatorDetails calculatorDetails) // use it as ctor parameter
+        {
+            this.calculatorDetails = calculatorDetails;
+        }
+        [When(@"I have entered ""(.*)"", ""(.*)"" and ""(.*)"" into the calculator and press logarithmic failure intensity")]
+        public void WhenIHaveEnteredAndIntoTheCalculatorAndPressLogarithmicFailureIntensity(double p0, double p1, int p2)
+        {
+            _result = calculatorDetails.calculator.CurrentFailureIntensityLogModel(p0, p1, p2);
+        }
+        
+        [When(@"I have entered ""(.*)"", ""(.*)"" and ""(.*)"" into the calculator and press logarithmic expected failure")]
+        public void WhenIHaveEnteredAndIntoTheCalculatorAndPressLogarithmicExpectedFailure(double p0, double p1, double p2)
+        {
+            _result = calculatorDetails.calculator.LogAverageNumberofExpectedFailures(p0, p1, p2);
+        }
+
+        [Then(@"the log failure intensity result should be ""(.*)""")]
+        public void ThenTheLogFailureIntensityResultShouldBe(decimal p0)
+        {
+            Assert.That(_result, Is.EqualTo(p0));
+        }
+
+        [Then(@"the log expected failure result should be ""(.*)""")]
+        public void ThenTheLogExpectedFailureResultShouldBe(int p0)
+        {
+            Assert.That(_result, Is.EqualTo(p0));
+        }
+
+    }
+}
